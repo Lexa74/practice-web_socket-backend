3b5270e8a4f7828fb88c06b7f511f05be913a631
admin <tany--96@mail.ru>
2024-08-10 15:40:17 +0300

    done

--- chat.js
+++ chat.js
@@ -1,21 +1,14 @@
-const chat = [
+module.exports = { chat: [
   {
     "id": "1a2b3c4d",
     "username": "Alex",
     "message": "Hello",
-    "isViewed": false
+    "isViewed": true
   },
   {
     "id": "5e6f7g8h",
     "username": "Alex",
     "message": "Hello world",
-    "isViewed": false
-  },
-  {
-    "id": "e67f3b66-a96f-461f-971c-eb01993e0530",
-    "username": "Alex",
-    "message": "as"
+    "isViewed": true
   }
-]
-
-module.exports.chat = chat
+] };
--- index.js
+++ index.js
@@ -5,24 +5,18 @@ const {chat} = require('./chat')
 
 const wss = new WebSocket.Server({ port: 8080 });
 
-// Загружаем историю чата из файла, если он существует
-// if (fs.existsSync('chat.js')) {
-//     chatHistory = JSON.parse(fs.readFileSync('chat.js', 'utf-8'));
-// }
-
 wss.on('connection', (ws) => {
-    // Отправляем историю чата новому клиенту
     ws.send(JSON.stringify(chat));
 
     ws.on('message', (message) => {
         const decodedMessage = JSON.parse(message.toString());
-        // if(message.type === "new") {
-        //     addMessage({decodedMessage})
-        // }
-        // if(message.type === "view") {
-        //     viewMessage({decodedMessage})
-        // }
-        addMessage({decodedMessage})
+        console.log(decodedMessage)
+        if(decodedMessage.type === "new") {
+            addMessage({decodedMessage})
+        }
+        if(decodedMessage.type === "view") {
+            viewMessage({decodedMessage})
+        }
     })
     ws.on('close', (code, reason) => {
         console.log(`Connection closed: ${code} - ${reason}`);
@@ -34,29 +28,28 @@ wss.on('connection', (ws) => {
 });
 
 function addMessage({decodedMessage}) {
-    // const newMessage = {
-    //     id: randomUUID(),
-    //     username: "Alex",
-    //     message: decodedMessage
-    // };
-    // const decodedMessage = JSON.parse(message.toString());
-    //
     const newMessage = {
         id: randomUUID(),
         username: decodedMessage.username || "Alex",
-        message: decodedMessage.message || "Hello world"
+        message: decodedMessage.message || "Hello world",
+        isViewed: false
     };
-
     chat.push(newMessage);
+
+    fs.writeFileSync('./chat.js', `module.exports = { chat: ${JSON.stringify(chat, null, 2)} };`);
+
     sendClients()
 }
 
 function viewMessage({decodedMessage}) {
     const msgId = decodedMessage.id
+    if(!msgId) {
+        return console.log("Не передан id")
+    }
     const msgIdx = chat.findIndex(el => el.id === msgId)
     chat[msgIdx].isViewed = true
 
-    // fs.writeFileSync('chat.js', JSON.stringify(chatHistory, null, 2));
+    fs.writeFileSync('./chat.js', `module.exports = { chat: ${JSON.stringify(chat, null, 2)} };`);
 
     sendClients()
 }
